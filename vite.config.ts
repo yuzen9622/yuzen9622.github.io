1340b485f6b159dc5ab31f3f7d0f5a49bacc9c2d
yuzen9622 <oscar48079@gmail.com>
2025-07-07 23:31:56 +0800

    fix?(vite.config):change basename try to fix while reloading 404

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/yuzen9622.github.io/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
