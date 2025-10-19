17ce828d22a38a52766677205304457545784acc
yuzen9622 <oscar48079@gmail.com>
2025-10-19 18:29:35 +0800

    fix: 移除未使用的 useMemo 引入

--- src/components/about/AboutSection/main.tsx
+++ src/components/about/AboutSection/main.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useMemo } from "react";
+
 import { useTranslation } from "react-i18next";
 import { Separator } from "@/components/ui/separator";
 
