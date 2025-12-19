f7e48ac2cbb43b157edf0c9c3f433137669bd59d
yuzen9622 <oscar48079@gmail.com>
2025-12-19 23:24:33 +0800

    feat: 在HomePage中新增SEO相關的meta標籤以提升搜尋引擎優化

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,6 +8,18 @@ export default function AboutPage() {
     <>
       <Helmet>
         <title>Yuzen - Developer</title>
+        <meta
+          name="description"
+          content="Yuzen 的個人網頁，展示開發作品、技能、專案與聯絡方式，專注於全端開發、網頁設計。"
+        />
+        <meta
+          name="keywords"
+          content="Yuzen, 全端開發, 前端, 後端, 個人網頁, 程式作品, 網頁設計, AI, Python,Portfolio, JavaScript, React, Next.js,small Z,Small Z"
+        />
+        <meta name="author" content="Yuzen" />
+        <meta property="og:image" content="/avatar.webp" />
+        <meta property="og:url" content="https://www.yuzen.dev/" />
+        <meta property="og:type" content="website" />
       </Helmet>
 
       <AboutHero />
